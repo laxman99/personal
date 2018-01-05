7e9b1f04fc6121d078011f8925882abbfdb8c5df
laxman99 <35124814+laxman99@users.noreply.github.com>
2018-01-05 09:09:12 +0545

    index.php
    this online change

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<!--
+Laxman Bhattarai Web Design, Website Designer, Software Development, Developer, Graphic Design, E-commerce, Best web Developer in Nepal, Domain Registration, Hosting, Website Development located in Kathmandu, Palpa, Lumbini, A best web Designer, Search Engine Optimized SEO, Best Developer, Designer in south Asia, Personal Website Development, Website, Hosting Service, Multimedia Presentation. Best Web Design for Travel Agent, Nepali Product, Handicrafts, Trekking Agent, School, College, News, Hotel & Restaurent Website Developer. Laxman Bhattarai Web Design, Website Developer Designer, Software Development, Graphic, Nepal, Hosting.
+-->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Laxman Bhattarai Web Design, Website Developer, Web Designer, Software Development, Graphic Design in Nepal, Web Hosting, Best web Developer in Nepal & South Asia, Content Management System Developer.</title>
+<meta name="Description" content="Laxman Bhattarai Web Design, Website Developer, Web Designer, Software Development, Developer, Graphic Design, E-commerce, Best web Developer in Nepal & South Asia, Domain Registration, Hosting, best design company in Nepal it located in Kathmandu, Nepal, Search Engine Optimized SEO, Personal Website Development, Multimedia Presentation, Web Developments, Best Web Design for Travel Agent, Nepali Product, Trekking Agent, School, College, News, Hotel & Restaurent Website Developer, Content Management System Developer.">
+<META name="Keywords" content="Laxman Bhattarai Web Design, Website Designer, Software, Developer, Development, Nepal, Content Management System (CMS) Developer, laxmanb, Web Development,.">
+<meta name="robots" content="All">
+<meta name="language" content="EN-US">
+<META name="y_key" content="d2d4d1d008afeb05">
+<meta name="msvalidate.01" content="5601DB2C719E4635B7AAA4FE74EBAE71" />
+
+<meta name="google-site-verification" content="6vfWdKnJEojyZgXgeIHzZdgfBq9lSdFVxq5YFopeGEw" />
+
+<meta http-equiv="pragma" content="Cache">
+<META name="distribution" content="Global">
+<meta name="revisit-after" content="1 Day">
+<meta name="rating" content="SAFE FOR KIDS">
+<meta name="document-type" content="Public">
+<META name="generator" content="dreamweaver">
+<meta name="author" content="Mr. Laxman Bhattarai">
+<META http-equiv="window-target" content="_self">
+<META http-equiv="content-language" content="en">
+<META name="Reply-to" content="info@laxmanb.com.np">
+<meta name="Reply-to" content="info@laxmanb.com.np">
+<META name="classification" content="Internet-Web Design, Personal Website">
+<meta name="copyright" content="Laxman Bhattarai- 2006">
+<meta http-equiv="bulletin-text" content="Laxman Bhattarai">
+<meta name="abstract" content="Laxman Bhattarai Web Design, Website Designer, Software Development, Developer, Graphic Design, E-commerce, Best web Developer in Nepal, Domain Registration, Website Development, best design company in Nepal it located in Kathmandu, Palpa, Lumbini, Search Engine Optimized SEO.">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> 
+<link href="laxman.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.style2 {
+	font-size: 1px;
+	color: #0B2236;
+}
+-->
+</style>
+<script type="text/javascript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+//-->
+</script>
+</head>
+<body onLoad="MM_preloadImages('images/home_bt11.jpg','images/aboutme_bt11.jpg','images/portfolio_1.jpg','images/guestbook.jpg','images/forum_bt1.jpg','images/photogallery.jpg','images/feedback_bt1.jpg','images/contact_me_11.jpg')">
+<table width="800" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td bgcolor="#0B2236">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#0B2236"><table width="760" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="137"><a href="http://www.laxmanb.com.np"><img src="images/laxman.jpg" alt="Laxman Bhattarai" width="89" height="60" hspace="5" border="0" /></a></td>
+        <td width="276" valign="bottom" class="ariallitewhite10">          <script language="JavaScript" type="text/javascript" class="news">
+<!-- //Begin
+var mydate=new Date()
+var year=mydate.getYear()
+var daym=mydate.getDate()
+var month=mydate.getMonth()
+if (daym<10)
+daym="0"+daym
+var montharray=new 
+Array('January','February','March','April','May','June','July','August', 'September','October','November','December')
+document.write(" "+montharray[month]+" "+daym+"th, "+year+" ")
+//End -->
+          </script>
+        </td>
+        <td width="347" valign="bottom"><table width="252" border="0" align="right" cellpadding="0" cellspacing="0">
+          <tr>
+            <td>&nbsp;</td>
+            <td height="20">&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td height="20">&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><a href="index.php"><img src="images/home.gif" alt="Home" width="54" height="12" border="0" /></a></td>
+            <td height="20"><a href="about.php"><img src="images/about_me.gif" alt="About Me" width="74" height="12" hspace="20" border="0" /></a></td>
+            <td><a href="contact.php"><img src="images/contact_me.gif" alt="Contact Me" width="84" height="12" hspace="0" border="0" /></a></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr> 
+  <tr>
+    <td bgcolor="#0B2236"><table width="800" border="0" align="center" cellpadding="0" cellspacing="0" class="arial11">
+      <tr>
+        <td width="20" height="12"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai Email Services."></td>
+        <td width="760" height="12" class="top_bgi"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai."></td>
+        <td width="20" height="12"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai."></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td bgcolor="#4A657D"><table width="740" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td height="10"><img src="images/trans.gif" alt="Laxman Bhattarai" width="1" height="1"></td>
+          </tr>
+          <tr>
+            <td><img src="laxman_banner.jpg" alt="Laxman Bhattarai, Web Design, Graphic Design, Software Development." width="741" height="151"></td>
+          </tr>
+          <tr>
+            <td height="10"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai."></td>
+          </tr>
+          
+          <tr>
+            <td height="21" background="images/menu_bg.gif"><table width="656" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td><a href="index.php"><img src="images/home_1.gif" alt="Home" width="39" height="21" hspace="10" border="0" /></a></td>
+                <td><a href="about.php"><img src="images/about_me1.gif" alt="About Me" width="68" height="21" hspace="10" border="0" /></a></td>
+                <td><img src="images/portfolio.gif" alt="Portfolio" width="75" height="21" hspace="10" /></td>
+                <td><a href="guest_book.php"><img src="images/guest_book.gif" alt="Guestbook" width="87" height="21" hspace="10" border="0" /></a></td>
+                <td><a href="webf/index.php"><img src="images/forum.gif" alt="Web Forum." width="47" height="21" hspace="10" border="0" /></a></td>
+                <td><img src="images/photogallery.gif" alt="Photogallery" width="106" height="21" hspace="10" /></td>
+                <td><img src="images/feedback.gif" alt="Feedback" width="71" height="21" hspace="10" /></td>
+                <td><a href="contact.php"><img src="images/contact_me_1.gif" alt="Contact Me" width="83" height="21" hspace="10" border="0" /></a></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td height="1" bgcolor="#D2DBE3"><img src="images/trans.gif" alt="Laxman Bhattarai." width="1" height="1"></td>
+          </tr>
+          <tr>
+            <td height="10">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="10">&nbsp;</td>
+          </tr>
+          <tr>
+            <td height="10"><img src="images/trans.gif" alt="Laxman Bhattarai." width="1" height="1"><img src="images/welcome.gif" alt="Welcome, Laxman Bhattarai" width="161" height="28"></td>
+          </tr>
+          <tr>
+            <td><table width="740" border="0" cellpadding="0" cellspacing="0" class="arial11"><tr>
+                <td width="270" height="15"><img src="images/trans.gif" alt="Laxman Bhattarai" width="1" height="1"></td>
+                <td width="470" rowspan="3" align="right" valign="bottom" bgcolor="#4A657D" class="arial11" id="rtem_bgi"><img src="images/laxman_next.jpg" alt="Laxman Bhattarai." width="219" height="301" hspace="0" vspace="0" border="0"></td>
+              </tr>
+              <tr>
+                <td height="15"><img src="images/laxman_logo.jpg" alt="Laxman Bhattarai Web Design, Best Website Developer in Nepal." width="200" height="112" border="0"></td>
+                </tr>
+              
+              <tr>
+                <td height="15"><p class="arialwhite12">I am Laxman Bhattarai from Kathmandu, Nepal. I am a Web Developer.<br>
+                  <br>
+                  I hope that you will enjoy visiting my website and come back often! I am in the process of adding more features and sections. Hopefully in the coming days I  will be able to make this site more informative and useful for Web Developer, Software Developer, Graphic Designer and some useful information about Content Management System (CMS). <a href="http://www.blog.laxmanb.com.np" style="font-family:Arial, Helvetica, sans-serif; font-size:11px; color:#CCCCCC;">View My BLOG</a></p>                  </td>
+                </tr>
+              </table></td>
+          </tr><tr>
+            <td height="1" bgcolor="697C8D"><img src="images/trans.gif" width="1" height="1" alt="Web Development"><img src="images/trans.gif" alt="Laxman Bhattarai." width="1" height="1"></td>
+          </tr>
+          <tr>
+            <td height="1" bgcolor="#8297A5"><img src="images/trans.gif" alt="Laxman Bhattarai." width="1" height="1"></td>
+          </tr>
+          <tr>
+            <td height="28"><script type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<table width="656" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td><a href="index.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Home','','images/home_bt11.jpg',1)"><img src="images/home_bt1.jpg" alt="Home" name="Home" width="63" height="28" border="0" id="Home" /></a></td>
+                <td><a href="about.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('About Me','','images/aboutme_bt11.jpg',1)"><img src="images/aboutme_bt1.jpg" alt="About Me" name="About Me" width="86" height="28" border="0" id="About Me" /></a></td>
+                <td><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Portfolio','','images/portfolio_1.jpg',1)"><img src="images/portfolio.jpg" alt="Portfolio" name="Portfolio" width="94" height="28" border="0" id="Portfolio" /></a></td>
+                <td><a href="guest_book.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('GuestBook','','images/guestbook.jpg',1)"><img src="images/guesebook_bt1.jpg" alt="Guest Book" name="GuestBook" width="106" height="28" hspace="1" border="0" id="GuestBook" /></a></td>
+                <td><a href="webf/index.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Forum','','images/forum_bt1.jpg',1)"><img src="images/forum_1bt.jpg" alt="Forum" name="Forum" width="63" height="28" hspace="1" border="0" id="Forum" /></a></td>
+                <td><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Photogallery','','images/photogallery.jpg',1)"><img src="images/photogallery_bt1.jpg" alt="Photogallery" name="Photogallery" width="125" height="28" hspace="1" border="0" id="Photogallery" /></a></td>
+                <td><a href="feedback.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Feedback','','images/feedback_bt1.jpg',1)"><img src="images/feedback.jpg" alt="Feedback" name="Feedback" width="87" height="28" hspace="1" border="0" id="Feedback" /></a></td>
+                <td><a href="contact.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Contact','','images/contact_me_11.jpg',0)"><img src="images/contact_bt1.jpg" alt="Contact Me" name="Contact" width="106" height="28" border="0" id="Contact" /></a><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Contact','','images/contact_me_11.jpg',1)"></a></td>
+              </tr>
+            </table></td>
+          </tr> </table></td>
+        <td background="images/bg_right.gif">&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="8"></td>
+        <td height="8"></td>
+        <td height="8"></td>
+      </tr><tr>
+        <td height="12">&nbsp;</td>
+        <td height="12" align="center" bgcolor="#0B2236"><script type="text/javascript"><!--
+google_ad_client = "pub-8402767471375282";
+google_ad_width = 728;
+google_ad_height = 90;
+google_ad_format = "728x90_as";
+google_ad_type = "text_image";
+google_ad_channel = "";
+google_color_border = "0B2236";
+google_color_bg = "0B2236";
+google_color_link = "6EA0CA";
+google_color_text = "C5DDF3";
+google_color_url = "008000";
+//-->
+</script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></td>
+        <td height="12">&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="12"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai Web Design, Best Website Developer in Nepal, Designer, Software Development, Developer, Graphic Design, Software, Hosting."></td>
+        <td height="12"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai." title="Laxman Bhattarai."></td>
+        <td height="12"><img src="images/trans.gif" width="1" height="1" alt="Laxman Bhattarai." title="Laxman Bhattarai."></td>
+      </tr>
+      <tr><td height="12">&nbsp;</td>
+        <td height="20"><link href="laxman.css" rel="stylesheet" type="text/css" />
+<table width="740" border="0" align="center" cellpadding="0" cellspacing="0" class="arial11">
+  
+  <tr>
+    <td width="556" height="15" class="ariallitewhite10">Copyright &copy; 2006, Laxman Bhattarai. All Right Reserved.. </td>
+    <td width="184" rowspan="4" class="ariallitewhite10">&nbsp;</td>
+  </tr>
+  
+  <tr>
+    <td height="15" class="ariallitewhite10">Kamaladi, Kathmandu Nepal </td>
+  </tr>
+  <tr>
+    <td height="15" class="ariallitewhite10"><b>Phone</b>  +977-9851127307, Mobile: +977-9841385955 </td>
+  </tr>
+  <tr>
+    <td height="15" class="ariallitewhite10">Website: www.laxmanb.com.np, Email: info@laxmanb.com.np</td>
+  </tr>
+</table>
+</td>
+        <td height="12">&nbsp;</td>
+      </tr>
+      
+    </table></td>
+  </tr>
+  
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
